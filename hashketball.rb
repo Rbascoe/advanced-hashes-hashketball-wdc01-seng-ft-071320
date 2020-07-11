2ff647fc7eb000fd3fbbbdd99d7d80417d7ba608
Roan Bascoe <roanbascoe5@gmail.com>
2020-07-11 11:38:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -174,7 +174,7 @@ end
 def player_stats(player_name)
   game_hash.each do |team_location, team_info|
       if team_info[:player_name] == player_name
-        return team_info[:players].each do |player_stats|
+        return team_info[:players].map do |player_stats|
           player_stats[:players].keys
       end
     end
